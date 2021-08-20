b3886e4b92049c8ed2a91308055eb2f120369d3f
zhuanghaji <zhuanghaji95@gmail.com>
2021-08-20 17:31:02 +0800

    初始化

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,41 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
 <style>
+*{
+        padding: 0;
+        margin: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+
+.container {
+  width: 100%;
+  padding: 0 15px;
+  margin: 0 auto;
 }
 </style>
---
+++ src/views/Header.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="header">
+    <div class="container">
+        <ul>
+            <li>首页</li>
+            <li>妹纸</li>
+            <li>干货</li>
+            <li>专题</li>
+            <li>Flutter</li>
+            <li>API</li>
+            <li>留言</li>
+        </ul>
+        <div id="title">
+            干货集中营
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang='less'>
+#header {
+    height: 8rem;
+    width: 100%;
+    background: black;
+    align-items: center;
+    display: flex;
+    ul {
+    order: 2;
+        display: flex;
+        list-style: none;
+        color: white;
+        &:hover {
+                color: rgb(82, 82, 82);
+                cursor: pointer;
+            }
+        li {
+            width: 3rem;
+            line-height: 2rem;
+            text-align: center;
+            &:hover {
+                color: #2c63ff;
+                cursor: pointer;
+            }
+        }
+    }
+    #title {
+        font-weight: 400;
+        font-size: 2.5rem;
+        color: white;
+        order: 1;
+    }
+}    
+
+
+
+</style>
